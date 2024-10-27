ec3b11e14117b2ca3e065dc63e319736f608e292
sonalvanave <vanvesonal@proton.me>
2024-10-27 09:18:31 +0000

    dogcomplete

--- src/main.js
+++ src/main.js
@@ -1,28 +1,40 @@
-/* API Endpoints
- * breeds list: https://dog.ceo/api/breeds/list/all
- * get single breed image: https://dog.ceo/api/breed/{breed}/images/random
- */
+// === DOM Selection
+const selectEl = document.querySelector("#dog-breeds");
+const imageEl = document.querySelector("#dog-image");
 
+// === Fetch
 // This function returns [breeds]
-// dom selection
-
-const selectE1 = document.querySelector(" #dog-breeds");
-console.log(selectE1);
-
 function getDogsBreed() {
-    return fetch(`https://dog.ceo/api/breeds/list/all`)
-      .then((res) => res.json())
-      .then((data) => Object.keys(data.message))
-      .catch((err) => console.log(err));
-  }
-  
-function renderoption(){
-    getDogsBreed().then((breeds)=> {
-        for(let breed of breeds){
-            const option = document.createElement("option");
-            option.textContent=breed;
-            selectE1.appendChild(option);
-        }
-    });
+  return fetch(`https://dog.ceo/api/breeds/list/all`)
+    .then((res) => res.json())
+    .then((data) => Object.keys(data.message))
+    .catch((err) => console.log(err));
 }
-renderoption();
+
+function getDogImage(breed) {
+  return fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then((res) => res.json())
+    .then((data) => data.message)
+    .catch((err) => console.log(err));
+}
+
+// === Render
+// Renders the <option>
+function renderOptions() {
+  getDogsBreed().then((breeds) => {
+    for (let breed of breeds) {
+      const option = document.createElement("option");
+      option.textContent = breed[0].toUpperCase() + breed.slice(1);
+      option.value = breed;
+      selectEl.appendChild(option);
+    }
+  });
+}
+
+renderOptions();
+
+selectEl.addEventListener("change", (e) => {
+  const selectedValue = e.target.value;
+
+  getDogImage(selectedValue).then((link) => (imageEl.src = link));
+});
